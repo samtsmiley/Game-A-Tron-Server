4514d7609323b8177f534368e61a4cd596f23cea
seankrummel <sean.krummel@gmail.com>
2019-02-12 10:54:23 -0600

    updated user model

--- users/models.js
+++ users/models.js
@@ -1,39 +1,32 @@
 'use strict';
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
-// const Game = require('../game');
-// const {words} = require('../data/words');
-//user.word.english
 
 mongoose.Promise = global.Promise;
 
-const UserSchema =  mongoose.Schema({
+const schema = new mongoose.Schema({
   username: {type: String, required: true,unique: true},
   password: {type: String, required: true},
-  // firstName: {type: String, default: ''},
-  // lastName: {type: String, default: ''},
-  // games: [{gameId: mongoose.Types.Object.Id, ref: Game}]
+  games: [{gameId: mongoose.Types.Object.Id, ref: 'Game'}]
 });
 
-UserSchema.methods.serialize = function() {
-  return {
-    username: this.username,
-    // firstName: this.firstName || '',
-    // lastName: this.lastName || '',
-    id: this._id,
-    games: this.games,
+schema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, result) => {
+    delete result._id;
+    delete result.__v;
+    delete result.password;
+  }
+});
 
-  };
-};
-
-UserSchema.methods.validatePassword = function(password) {
+schema.methods.validatePassword = function(password) {
   return bcrypt.compare(password, this.password);
 };
 
-UserSchema.statics.hashPassword = function(password) {
+schema.statics.hashPassword = function(password) {
   return bcrypt.hash(password, 10);
 };
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', schema);
 
 module.exports = {User};
