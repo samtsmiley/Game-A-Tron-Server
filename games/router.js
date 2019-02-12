6f38020c0bd76d146dc0a3a2d670d7070908806e
seankrummel <sean.krummel@gmail.com>
2019-02-12 13:57:46 -0600

    created post game endpoint

--- games/router.js
+++ games/router.js
@@ -24,3 +24,49 @@ router.get('/:id', (req, res, next) => {
       else next();
     }).catch(err => next(err));
 });
+router.post('/', (req, res, next) => {
+  const {name, description, rules, scores} = req.body;
+  const userId = req.user.id;
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  const newGame = {name};
+  if (description) {
+    if (typeof description !== 'string' || !(description instanceof String)) {
+      const err = new Error('The `description` property must be a String');
+      err.status = 400;
+      return next(err);
+    }
+    newGame.description = description;
+  }
+  if (rules) {
+    if (!Array.isArray(rules) || !rules.every(rule => {
+      return typeof rule === 'string' || rule instanceof String;
+    })) {
+      const err = new Error('The `rules` property must be an Array of Strings');
+      err.status = 400;
+      return next(err);
+    }
+    newGame.rules = rules;
+  }
+  if (scores) {
+    // check we have the correct key/value pairs
+    if (!Array.isArray(scores) || !scores.every(score => {
+      // check if every rule is an Object that has the key 'description'
+      if (!(typeof rule === 'object' && score.constructor === Object)) return false;
+      return score.hasOwnProperty('description');
+    })) {
+      const err = new Error('The `scores` property must be an Array of Objects with a key `description`');
+      err.status = 400;
+      return next(err);
+    }
+    // make sure there aren't any extra key/value pairs
+    newGame.scores = scores.map(score => ({description: score.description, points: score.points}));
+  }
+
+  Game.create(newGame)
+    .then(result => res.location(`${req.originalUrl}/${result.id}`).status(201))
+    .catch(err => next(err));
+});
