a4251227c177ce4da388fd2861e9a4fa6db9fd19
seankrummel <sean.krummel@gmail.com>
2019-02-12 15:13:53 -0600

    changed game model and created put endpoint

--- games/models.js
+++ games/models.js
@@ -8,7 +8,8 @@ const schema = new mongoose.Schema({
   scores: [{description: {type: String, required: true}, points: Number}],
   endScore: {type: Number, default: null},
   posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}],
-  participants: [{userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}, score: Number, admin: Boolean}]
+  participants: [{userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}, score: Number}],
+  admins: [{userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}}]
 });
 
 schema.set('timestamps', true);
--- games/router.js
+++ games/router.js
@@ -32,7 +32,12 @@ router.post('/', (req, res, next) => {
     err.status = 400;
     return next(err);
   }
-  const newGame = {name};
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    const err = new Error('The `userId` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+  const newGame = {name: name.trim(), admins: [userId]};
   if (description) {
     if (typeof description !== 'string' || !(description instanceof String)) {
       const err = new Error('The `description` property must be a String');
@@ -69,4 +74,69 @@ router.post('/', (req, res, next) => {
   Game.create(newGame)
     .then(result => res.location(`${req.originalUrl}/${result.id}`).status(201))
     .catch(err => next(err));
+});
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const userId = req.user.id;
+  if (mongoose.Types.ObjectId.isValid(userId)) {
+    const err = new Error('The `userId` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+  const toUpdate = {};
+  const updatableFields = ['name', 'description', 'rules', 'scores'];
+  updatableFields.forEach(field => {
+    if (field in req.body) toUpdate[field] = req.body[field];
+  });
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+  if (toUpdate.name) {
+    toUpdate.name = toUpdate.name.trim;
+    if (typeof toUpdate.name !== 'string' || !(toUpdate.name instanceof String)) {
+      const err = new Error('The `name` property must be a String');
+      err.status = 400;
+      return next(err);
+    }
+  }
+  if (toUpdate.name === '') {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  if (toUpdate.description) {
+    if (typeof toUpdate.description !== 'string' || !(toUpdate.description instanceof String)) {
+      const err = new Error('The `description` property must be a String');
+      err.status = 400;
+      return next(err);
+    }
+  }
+  if (toUpdate.rules || toUpdate.rules === []) { // check if empty array because maybe user wants to remove all rules
+    if (!Array.isArray(toUpdate.rules) || !toUpdate.rules.every(rule => {
+      return typeof rule === 'string' || rule instanceof String;
+    })) {
+      const err = new Error('The `rules` property must be an Array of Strings');
+      err.status = 400;
+      return next(err);
+    }
+  }
+  if (toUpdate.scores || toUpdate.scores === []) { // check if empty array, same reason as above
+    if (!Array.isArray(toUpdate.scores) || !toUpdate.scores.every(score => {
+      if (!(typeof rule === 'object' && score.constructor === Object)) return false;
+      return score.hasOwnProperty('description');
+    })) {
+      const err = new Error('The `scores` property must be an Array of Objects with a key `description`');
+      err.status = 400;
+      return next(err);
+    }
+    toUpdate.scores = toUpdate.scores.map(score => ({description: score.description, points: score.points}));
+  }
+
+  Game.findOneAndUpdate({_id: id, admins: userId}, toUpdate, {new: true})
+    .then(result => {
+      if (result) res.json(result);
+      else next();
+    }).catch(err => next(err));
 });
