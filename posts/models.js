97b86ea7b3b24b32ab16873a8266abdf6dea09bd
seankrummel <sean.krummel@gmail.com>
2019-02-12 10:22:25 -0600

    created post model

---
+++ posts/models.js
@@ -0,0 +1,19 @@
+'use strict';
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  description: {type: String, required: true},
+  userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  value: Number
+});
+
+schema.set('timestamps', true);
+schema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, result) => {
+    delete result._id;
+    delete result.__v;
+  }
+});
+
+module.exports = {Post: mongoose.model('Post', schema)};
