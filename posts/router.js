a572e69c86db7bb78e29e3f8a74b7e63e13e3cdb
seankrummel <sean.krummel@gmail.com>
2019-02-13 10:14:20 -0600

    created get all user posts endpoint

--- posts/router.js
+++ posts/router.js
@@ -8,7 +8,14 @@ const {Post} = require('./models');
 
 router.use('/', passport.authenticate('jwt', {session: false, failWithError: true}));
 
-router.get('/', (req, res, next) => {});
+router.get('/', (req, res, next) => {
+  // get all posts for a user
+  const userId = req.query.userId;
+
+  Post.find({userId}).sort({updatedAt: 'desc'})
+    .then(results => res.json(results))
+    .catch(err => next(err));
+});
 router.get('/:id', (req, res, next) => {});
 router.post('/', (req, res, next) => {});
 router.put('/:id', (req, res, next) => {});
