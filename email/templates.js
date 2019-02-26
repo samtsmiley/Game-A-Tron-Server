99920f562e05de1562966e189aa807810bd69091
seankrummel <sean.krummel@gmail.com>
2019-02-26 14:08:33 -0600

    anchor tag was not working, changed to just having a link as text

--- email/templates.js
+++ email/templates.js
@@ -4,11 +4,12 @@ const {CLIENT_ORIGIN} = require('../config');
 function confirm(id) {
   return {
     subject: 'Confirm Email',
-    html: `
-      <a href='${CLIENT_ORIGIN}/confirm/${id}>
-        click to confirm email
-      </a>
-    `,
+    // html: `
+    //   <a href='${CLIENT_ORIGIN}/confirm/${id}>
+    //     click to confirm email
+    //   </a>
+    // `,
+    html: `Please click the link to confirm your email: ${CLIENT_ORIGIN}/confirm/${id}`,
     text: `Copy and paste this link: ${CLIENT_ORIGIN}/confirm/${id}`
   };
 }
